2f059839b76253e71fd24718bdc0757d88e300f1
Arun Kumar R <aruntutter19@gmail.com>
2024-01-26 12:38:32 +0530

    click event added

--- script.js
+++ script.js
@@ -51,3 +51,42 @@ function crossClicked() {
 }
 
 crossTwo.addEventListener("click", crossClicked);
+
+function toggleView() {
+  let viewAllTolls = document.getElementById("view-all-tolls");
+  let outputTitle = document.getElementById("output-title");
+  let filterInput = document.getElementById("filter-input");
+
+  if (viewAllTolls.innerText === "View all tolls") {
+    viewAllTolls.innerText = "Back to vehicle logs";
+    filterInput.innerHTML = `<label>Tollgate List</label>
+    <input type="text" placeholder="Search a Vehicle">`;
+    outputTitle.innerHTML = `
+        <h4 id="toll-name">TOLL NAME</h4>
+        <h4 id="vehicle-name">CAR/JEEP/VAN</h4>
+        <h4 id="lcv">LCV</h4>
+        <h4 id="truck-bus">TRUCK/BUS</h4>
+        <h4 id="heavy-vehicle">HEAVY VEHICLE</h4>
+      `;
+  } else {
+    viewAllTolls.innerText = "View all tolls";
+    filterInput.innerHTML = `<label>Toll Entries/Vehicle Entries</label>
+        <div class="dropdown">
+          <i class="fas fa-filter" id="filter-icon"></i>
+          <div class="dropdown-content dropdown-none" id="dropdown-content">
+            <a href="#">All</a>
+            <a href="#">Kappalur</a>
+            <a href="#">Chengalpattu</a>
+            <a href="#">Krishnagiri</a>
+          </div>
+        </div>
+        <input type="text" placeholder="Search a Vehicle">`;
+    outputTitle.innerHTML = `
+        <h4 id="toll-name">VEHICLE TYPE</h4>
+        <h4 id="vehicle-name">VEHICLE NUMBER</h4>
+        <h4 id="lcv">DATE/TIME</h4>
+        <h4 id="truck-bus">TOLL NAME</h4>
+        <h4 id="heavy-vehicle">TARIFF</h4>
+      `;
+  }
+}
